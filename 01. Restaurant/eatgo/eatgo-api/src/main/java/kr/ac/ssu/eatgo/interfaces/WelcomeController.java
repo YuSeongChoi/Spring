41d31a179bd93486073386d934a9604d29e23f2f
YuSeongChoi <marine8743@naver.com>
2020-03-23 18:49:02 +0900

    Spring Setting

--- 01. Restaurant/eatgo/eatgo-api/src/main/java/kr/ac/ssu/eatgo/interfaces/WelcomeController.java
+++ 01. Restaurant/eatgo/eatgo-api/src/main/java/kr/ac/ssu/eatgo/interfaces/WelcomeController.java
@@ -1,4 +1,4 @@
-package kr.ac.ssu.street4.interfaces;
+package kr.ac.ssu.eatgo.interfaces;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class WelcomeController {
 
     @GetMapping("/")
-    public String hello() {return "Hello, World! I'm YuSeong";}
-
+    public String hello() {
+        return "Hello, world!";
+    }
 }
