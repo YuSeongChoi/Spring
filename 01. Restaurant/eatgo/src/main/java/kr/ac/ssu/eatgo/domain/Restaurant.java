3c09dd0677bcb82e1f30fe47c29a4ade739ca051
YuSeongChoi <marine8743@naver.com>
2020-03-23 20:07:43 +0900

    TDD

---
+++ 01. Restaurant/eatgo/src/main/java/kr/ac/ssu/eatgo/domain/Restaurant.java
@@ -0,0 +1,8 @@
+package kr.ac.ssu.eatgo.domain;
+
+public class Restaurant {
+
+    public String getName() {
+        return "";
+    }
+}
---
+++ 01. Restaurant/eatgo/src/test/java/kr/ac/ssu/eatgo/domain/RestaurantTests.java
@@ -0,0 +1,15 @@
+package kr.ac.ssu.eatgo.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RestaurantTests {
+
+    @Test
+    public void creation() {
+        Restaurant restaurant = new Restaurant();
+    }
+
+}
