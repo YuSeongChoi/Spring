74762cf993bf1a5fcfdc0b71fcc479a76c3cae56
YuSeongChoi <marine8743@naver.com>
2020-03-24 19:49:07 +0900

    가게 목록

---
+++ 01. Restaurant/eatgo/src/test/java/kr/ac/ssu/eatgo/interfaces/RestaurantControllerTest.java
@@ -0,0 +1,36 @@
+package kr.ac.ssu.eatgo.interfaces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.core.StringContains.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@WebMvcTest(RestaurantController.class)
+class RestaurantControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void list() {
+        try {
+            mvc.perform(get("/restaurants"))
+                    .andExpect(status().isOk())
+                    .andExpect(content().string(
+                            containsString("\"id\":1004")
+                    ))
+                    .andExpect(content().string(
+                            containsString("\"name\":\"Bob zip\"")
+                    ));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
